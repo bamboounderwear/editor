ed61759ad228f5eca080fdc5892fe2f222991c49
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2025-02-19 13:43:39 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     port: 5173
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        email: resolve(__dirname, 'public/templates/email.html'),
-        instagram: resolve(__dirname, 'public/templates/instagram.html'),
-        slide: resolve(__dirname, 'public/templates/slide.html')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
   }
 });
